add97ec052b3355fad2010ffb713510cf5cf9625
phoblak <6821901022@cdti.ac.th>
2026-06-22 10:32:12 +0700

    update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,14 @@
 export default function Login(){
     return(
-        <div>
-            <h1>เข้าสู่ระบบ</h1>
-            <input type="Email" placeholder="อีเมล"/>
-            <input type="Password" placeholder="รหัส"/>
-            <a href="/">
+        <div className="auth-page">
+            <form className="auth-card">
+                <h1>เข้าสู่ระบบ</h1>
+                <input type="Email" placeholder="อีเมล"/>
+                <input type="Password" placeholder="รหัส"/>
+                <a href="/">
                 <button>Login</button>
-            </a>
-            
+                </a>
+            </form>
         </div>
     )
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,12 +1,45 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Register(){
+
+    const router = useRouter();
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify(form),
+    });
+
+    if (res.ok) {
+        alert("ลงทะเบียนสำเร็จ");
+      router.push("/login");
+    } else {
+      alert("Register failed");
+    }
+  }
+
     return(
         <div className="auth-page">
-            <form className="auth-card">
+            <form className="auth-card" onSubmit={handleSubmit}>
                 <h1>สมัครสามาชิก</h1>
-                <input type="name" placeholder="ชื่อ-นามสกุล"/>
-                <input type="Password" placeholder="รหัส"/>
-                <input type="Email" placeholder="อีเมล"/>
-                <a href="app/login/page.tsx">
+                <input type="name" placeholder="ชื่อ-นามสกุล"
+                onChange={(e) => setForm({ ...form, name: e.target.value })}/>
+                <input type="Password" placeholder="รหัส"
+                onChange={(e) => setForm({ ...form, password: e.target.value })}/>
+                <input type="Email" placeholder="อีเมล"
+                onChange={(e) => setForm({ ...form, email: e.target.value })}/>
+                <a href="">
                     <button>Register</button>
                 </a>
             </form>
